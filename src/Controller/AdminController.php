4584383927c1979ae43ed9fce299ba9816de7fa6
eliocartier <68470195+eliocartier@users.noreply.github.com>
2020-10-13 14:12:42 +0200

    Creation des routes et fichiers twig dans AdminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,14 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class AdminController
+ * @package App\Controller
+ * @Route("/admin")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route ("/villes", name="edit_villes")
      */
-    public function index()
+    public function editVilles()
     {
-        return $this->render('admin/login.html.twig', [
+        return $this->render('admin/editvilles.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route ("/campus", name="edit_campus")
+     */
+    public function editCampus()
+    {
+        return $this->render('admin/editcampus.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
