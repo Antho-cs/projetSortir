24f743a69bc4143b128425d66e2f721b8b24f8f5
eliocartier <68470195+eliocartier@users.noreply.github.com>
2020-10-16 18:07:40 +0200

    Add fonction ajout campus et villes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,8 @@
-
-
 window.addEventListener("load", function(event) {
     let heroSection = document.getElementById('hero');
     let heroContainter = document.getElementsByClassName('hero-container');
 });
+
+document.getElementById("addCampus").onclick = function() {
+    document.getElementById("inputCampus").submit();
+};
--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
+use App\Entity\Villes;
+use App\Form\AddCampusType;
+use App\Form\AddVillesType;
 use App\Form\RechercheCampusType;
 use App\Form\RechercheVilleType;
 use App\Repository\CampusRepository;
 use App\Repository\VillesRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,10 +26,13 @@ class AdminController extends AbstractController
     /**
      * @Route ("/villes", name="edit_villes")
      */
-    public function editVilles(Request $request, VillesRepository $villesRepository)
+    public function editVilles(Request $request, VillesRepository $villesRepository, EntityManagerInterface $em)
     {
         $villesForm = $this->createForm(RechercheVilleType::class);
         $villesForm->handleRequest($request);
+
+        $addVillesForm = $this->createForm(AddVillesType::class);
+        $addVillesForm->handleRequest($request);
 
         if($villesForm->isSubmitted() && $villesForm->isValid()){
             $recherche = $villesForm['nomVille']->getData();
@@ -32,20 +41,37 @@ class AdminController extends AbstractController
             $listeVilles = $villesRepository->findAll();
         }
 
+        if ($addVillesForm->isSubmitted() && $addVillesForm->isValid()) {
+            $newVilles = new Villes();
+            $nomVille = $addVillesForm['nomVille']->getData();
+            $codePostal = $addVillesForm['codePostal']->getData();
+            $newVilles->setNomVille($nomVille);
+            $newVilles->setCodePostal($codePostal);
+            $em->persist($newVilles);
+            $em->flush();
+            $this->addFlash('success', 'La ville a bien été créé !');
+
+            return $this->redirectToRoute('edit_villes');
+        }
+
         return $this->render('admin/editvilles.html.twig', [
             'controller_name' => 'AdminController',
             'listeVilles' => $listeVilles,
-            'villesForm' => $villesForm->createView()
+            'villesForm' => $villesForm->createView(),
+            'addVillesForm' => $addVillesForm->createView()
         ]);
     }
 
     /**
      * @Route ("/campus", name="edit_campus")
      */
-    public function editCampus(Request $request, CampusRepository $campusRepository)
+    public function editCampus(Request $request, CampusRepository $campusRepository, EntityManagerInterface $em)
     {
         $campusForm = $this->createForm(RechercheCampusType::class);
         $campusForm->handleRequest($request);
+
+        $addCampusForm = $this->createForm(AddCampusType::class);
+        $addCampusForm->handleRequest($request);
 
         if($campusForm->isSubmitted() && $campusForm->isValid()){
             $recherche = $campusForm['nomCampus']->getData();
@@ -53,10 +79,23 @@ class AdminController extends AbstractController
         } else {
             $listeCampus = $campusRepository->findAll();
         }
+
+        if ($addCampusForm->isSubmitted() && $addCampusForm->isValid()) {
+            $newCampus = new Campus();
+            $nomCampus = $addCampusForm['nomCampus']->getData();
+            $newCampus->setNomCampus($nomCampus);
+            $em->persist($newCampus);
+            $em->flush();
+            $this->addFlash('success', 'Le campus est bien créé !');
+
+            return $this->redirectToRoute('edit_campus');
+        }
+
         return $this->render('admin/editcampus.html.twig', [
             'controller_name' => 'AdminController',
             'listeCampus' => $listeCampus,
-            'campusForm' => $campusForm->createView()
+            'campusForm' => $campusForm->createView(),
+            'addCampusForm' => $addCampusForm->createView()
         ]);
     }
 }
